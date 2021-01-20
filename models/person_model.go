99a4f4999e644ee8eedb9f83863e5fb8612331d6
unknown <siddhantjain15298@gmail.com>
2021-01-20 13:14:30 +0530

    Rest Api With golang and mongodb

--- models/person_model.go
+++ models/person_model.go
@@ -7,7 +7,7 @@ import
 type Person struct {
 	ID          primitive.ObjectID 	`json:"_id,omitempty" bson:"_id,omitempty"`
 	Name 		string 				`json:"Name" bson:"name"`
-	Address 	string				`json:"Address bson:"Address"`
+	Address 	string				`json:"Address" bson:"Address"`
 	CreatedAt 	time.Time			`json:"CreatedAt" bson:"CreatedAt"`
 	UpdatedAt 	time.Time 			`json:"UpdatedAt" bson:"UpdatedAt"`
 }
